203675259d02d7f88de555d15639da1d082b766f
Leonardo Olvera <leonardo.olvera34@my.unitec.edu.mx>
2026-03-19 18:22:44 -0600

    init

---
+++ src/Application262E16/Class1.cs
@@ -0,0 +1,12 @@
+﻿public class Figura262E16 {
+public static double Perimetro(double radio) {
+return 2*(3.14*radio);
+}
+public static double Area(double radio ) {
+return 3.14*(radio*radio);
+}
+public static double Volumen(double area, double altura ){
+return area*altura; 
+     
+}
+}
---
+++ src/ConsoleApp262E16/Program.cs
@@ -0,0 +1,9 @@
+﻿//volumen de un cilindro  es (3.14*radio*radio*altura)=area*altura
+double radio = 5;
+double altura=10;
+double perimetro = Figura262E16.Perimetro(radio);
+double area = Figura262E16.Area(radio);
+double volumen = Figura262E16.Volumen(area, altura);
+Console.WriteLine($"El perímetro de un circulo con radio de {radio}cm  es {perimetro:F2}cm");
+Console.WriteLine($"El área de un circulo de {radio}cm es {area}cm2");
+Console.WriteLine($"El volumen de un cilindro con area de {area}cm2 y con una altura de {altura}cm  es {volumen}cm3");
