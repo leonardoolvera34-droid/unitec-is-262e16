80fbd48406e639c2473d5be5252a488eff26592d
Leonardo Olvera <leonardo.olvera34@my.unitec.edu.mx>
2026-03-26 19:25:26 -0600

    int

--- src/Application262E16/Class1.cs
+++ src/Application262E16/Class1.cs
@@ -1,9 +1,9 @@
 ﻿public class Figura262E16 {
 public static double Perimetro(double radio) {
-return 2*(3.14*radio);
+return 2*(Math.PI*radio);
 }
 public static double Area(double radio ) {
-return 3.14*(radio*radio);
+return Math.PI*(radio*radio);
 }
 public static double Volumen(double area, double altura ){
 return area*altura; 
